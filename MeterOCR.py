047ee9292503f7fdc337b5d4dd5b429879ab8505
tuiho0706proton <87801152+tuiho0706proton@users.noreply.github.com>
2021-08-23 18:00:09 +0700

    Add files via upload

---
+++ MeterOCR.py
@@ -0,0 +1,89 @@
+# USAGE
+# python yolo.py --image images/baggage_claim.jpg --yolo yolo-coco
+
+# import the necessary packages
+import numpy as np
+import argparse
+import time
+import cv2
+import os
+
+
+configPath = "models/box.cfg"
+weightsPath = "models/box.weights"
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+img_dir = "./image"
+f_lists = os.listdir(img_dir)
+for f_name in f_lists:
+	f_path = img_dir+"/"+f_name
+
+	# load our input image and grab its spatial dimensions
+	image = cv2.imread(f_path)
+	(H, W) = image.shape[:2]
+
+	# determine only the *output* layer names that we need from YOLO
+	ln = net.getLayerNames()
+	ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+	# construct a blob from the input image and then perform a forward
+	# pass of the YOLO object detector, giving us our bounding boxes and
+	# associated probabilities
+	blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (300, 200), swapRB=True, crop=False)
+	net.setInput(blob)
+	start = time.time()
+	layerOutputs = net.forward(ln)
+	end = time.time()
+
+	# show timing information on YOLO
+	print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+	# initialize our lists of detected bounding boxes, confidences, and
+	# class IDs, respectively
+	boxes = []
+	confidences = []
+	classIDs = []
+	CONFIDENCE = 0.5
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		# loop over each of the detections
+		for detection in output:
+			# extract the class ID and confidence (i.e., probability) of
+			# the current object detection
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+
+			# filter out weak predictions by ensuring the detected
+			# probability is greater than the minimum probability
+			if confidence >= 0.3:
+				# scale the bounding box coordinates back relative to the
+				# size of the image, keeping in mind that YOLO actually
+				# returns the center (x, y)-coordinates of the bounding
+				# box followed by the boxes' width and height
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+
+				# use the center (x, y)-coordinates to derive the top and
+				# and left corner of the bounding box
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+
+				# update our list of bounding box coordinates, confidences,
+				# and class IDs
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+				res_img = cv2.rectangle(image, (x, y), (x+width, y+height), (0,0,255), 4)
+
+				#cv2.imwrite(f_name + '.png', res_img)
+
+		# hh, ww, _ = res_img.shape
+		show_img = cv2.resize(res_img, (600, 400))
+		cv2.imshow("res", show_img)
+		cv2.waitKey(0)
+		# cv2.imshow("result image", res_img)
+		# cv2.waitKey(0)
+	ppp = 0
